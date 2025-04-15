cd87d31cd6610a1e1672ff7d2d2ca073d65c0333
MaxteinXHH <1265884578@qq.com>
2025-04-15 22:17:03 +0800

    Update script.js

--- web/script.js
+++ web/script.js
@@ -22,7 +22,7 @@ const noTexts = [
   "？你认真的吗…",
   "要不再想想？",
   "不许选这个！ ",
-  "我会很伤心…",
+  "伤心…",
   "不行:(",
 ];
 
@@ -49,11 +49,11 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/hug.png"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/hug.png"; // 思考
-  if (clickCount === 3) mainImage.src = "images/hug.png"; // 生气
-  if (clickCount === 4) mainImage.src = "images/hug.png"; // 哭
-  if (clickCount >= 5) mainImage.src = "images/hug.png"; // 之后一直是哭
+  if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
+  if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
+  if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
+  if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面
